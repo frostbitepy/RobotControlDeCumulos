9a6ff33bfb0ebda40d6ef53f027cb7a458c25b8e
Francisco José Ruiz Vera <francisco@regionalseguros.com.py>
2023-10-03 16:32:41 -0300

    Log file added

---
+++ automation_scripts/script_logger.py
@@ -0,0 +1,18 @@
+import logging
+
+# Create a logger object
+logger = logging.getLogger(__name__)
+
+# Configure the logger
+logger.setLevel(logging.INFO)
+
+# Create a file handler and set the log level
+file_handler = logging.FileHandler('script.log')
+file_handler.setLevel(logging.INFO)
+
+# Create a formatter and attach it to the handler
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+
+# Add the handler to the logger
+logger.addHandler(file_handler)
